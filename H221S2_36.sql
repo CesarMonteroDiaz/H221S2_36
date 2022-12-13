51757c64305497eec5bc1976130083b759dee60e
CésarMonteroDíaz <111783490+CesarMonteroDiaz@users.noreply.github.com>
2022-12-13 17:36:05 -0500

    Update H221S2_36.sql

--- H221S2_36.sql
+++ H221S2_36.sql
@@ -4,7 +4,7 @@ USE IE;
 DROP TABLE informacion;
 CREATE TABLE informacion (
     IDINFO int AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT 'Contiene el identificador que controla el numero de informacion que se registra para su posterior respuesta.',
-    DOCINFO CHAR(12) NOT NULL COMMENT 'Contiene el numero de documento de indentidad para diferenciarlo el DNI=8 digitos y el carnet de estranjería =12 digitos',
+    DOCINFO CHAR(9) NOT NULL COMMENT 'Contiene el numero de documento de indentidad para diferenciarlo el DNI=8 digitos y el carnet de estranjería =12 digitos',
     APENOMINFO Varchar(80) NOT NULL COMMENT 'Contiene los apellidos y nombres del solicitante',
     EMAINFO Varchar(80) NOT NULL COMMENT 'Contiene el correo electronico del usuario solicitante para poder dar respuesta a su pregunta.',
     CELINFO Char(9) NOT NULL COMMENT 'Contiene el numero de celular del solicitante',
